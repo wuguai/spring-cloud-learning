9ef11cc9e36d81d90bf60deb47d37e93f926da1d
wuguai <chounong@126.com>
2020-02-16 11:48:41 +0800

    add service-provider-user

---
+++ service-provider-user/src/main/java/com/wendy/cloud/ServiceProviderUserApplication.java
@@ -0,0 +1,13 @@
+package com.wendy.cloud;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ServiceProviderUserApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ServiceProviderUserApplication.class, args);
+	}
+
+}
---
+++ service-provider-user/src/test/java/com/wendy/cloud/ServiceProviderUserApplicationTests.java
@@ -0,0 +1,13 @@
+package com.wendy.cloud;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ServiceProviderUserApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
